685a5821a9fde8c58e546273f00a74bc790e2ebf
novafacing <rowanbhart@gmail.com>
2020-12-20 02:32:49 -0500

    Update day 1 with comments

--- 2020/1/1.1.hs
+++ 2020/1/1.1.hs
@@ -2,11 +2,18 @@ import System.IO
 import Data.List
 
 
+-- Constrain TYPE a to be in Num and Eq
+-- We take a list of type a and an instance of type a and return an instance of type a
 twoSum :: (Num a, Eq a) => [a] -> a -> a
+-- return the product of two numbers from l1 whose sum is num
 twoSum l1 num = head [x * y | x <- l1, y <- l1, x + y == num]
 
 main = do
+    -- Read the file and split it by lines into a list
     contents <- fmap lines $ readFile "input"
+    -- Map "read" to the list (note we specify it must be [Int] type
     let report = (map read contents) :: [Int]
+        -- Use previous var as args to twoSum function
         match = twoSum report 2020
+    -- Display it!
     print match
--- 2020/1/1.2.hs
+++ 2020/1/1.2.hs
@@ -3,6 +3,7 @@ import Data.List
 
 
 threeSum :: (Num a, Eq a) => [a] -> a -> a
+-- Very similar to twoSum (this is NOT a good sol for large inputs!) 
 threeSum l1 num = head [x * y  * z | x <- l1, y <- l1, z <- l1, x + y + z == num]
 
 main = do
