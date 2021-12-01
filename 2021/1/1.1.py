3b39ad5f2f6fc90e06c7d0b94efc1dece67fe277
novafacing <rowanbhart@gmail.com>
2021-11-30 19:17:49 -1000

    Fix solutions to be good

--- 2021/1/1.1.py
+++ 2021/1/1.1.py
@@ -1,6 +1,7 @@
 """Advent of code 2021 Day 1.1"""
 
 from pathlib import Path
+from functools import reduce
 
 class Day1_1:
     def __init__(self, input_file: Path) -> None:
@@ -8,22 +9,24 @@ class Day1_1:
         self.data = list(map(int, input_file.open("r").read().splitlines()))
     
     def solution_1(self) -> int:
-        incs = 0
-        for i, dp in enumerate(self.data):
-            if i > 0 and dp > self.data[i-1]:
-                incs += 1
-        return incs
+        return sum(
+            map(
+                lambda i: 1 if (
+                    self.data[i-1] < self.data[i]
+                ) else 0,
+                range(1, len(self.data))
+            )
+        )
 
     def solution_2(self) -> int:
-        incs = 0
-        for i in range(3, len(self.data)):
-            l = self.data[i-4:i-1]
-            r = self.data[i-3:i]
-            if sum(l) < sum(r):
-                incs += 1
-        return incs
-
-
+        return sum(
+            map(
+                lambda i: 1 if (
+                    sum(self.data[i-4:i-1]) < sum(self.data[i-3:i])
+                ) else 0,
+                range(3, len(self.data))
+            )
+        )
 
 print(Day1_1(Path(__file__).with_name("1.1.input")).solution_1())
 print(Day1_1(Path(__file__).with_name("1.1.input")).solution_2())
